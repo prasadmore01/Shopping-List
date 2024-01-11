12cb480d2977c27ce0833d576059708542bf3f71
Prasad More <prasadmoremain@gmail.com>
2024-01-11 17:50:13 +0530

    Created JS file and Linked it in html

---
+++ app.js
@@ -0,0 +1,84 @@
+  
+        let arr=[]
+        let str=''
+        
+        let addBtn = document.querySelector('#addBtn');
+        addBtn.addEventListener('click',addFun,false)
+        
+        let show = document.querySelector('ul')
+        show.addEventListener('click',funClick,false)
+
+        let updateBtn = document.querySelector('#updateBtn')
+        updateBtn.addEventListener('click',updateFun,false)
+
+        let deleteBtn = document.querySelector('#deleteBtn')
+        deleteBtn.addEventListener('click',deleteFun,false)
+
+      
+
+
+        function Products(name,price){
+            this.name = name;
+            this.price = price;
+        }
+
+        function addFun(){
+            let name = document.getElementById('name').value;
+            let price = document.getElementById('price').value;
+            
+            let obj = new Products(name,price)
+            arr.push(obj)
+            display()
+        }
+
+        function display(){
+            str=""
+
+            for(let i=0;i<arr.length;i++){
+                str += "<li>"+arr[i].name+"="+arr[i].price+"</li>"
+            }
+
+            show.innerHTML=str
+            
+        }
+
+        function funClick(event){
+            let data = event.target.textContent;
+
+            let newArr = data.split('=')
+            console.log(newArr)
+            document.getElementById('name').value = newArr[0];
+            document.getElementById('price').value = newArr[1];
+
+            document.getElementById('name').readOnly=true;
+
+        }
+
+        function updateFun(){
+
+            let name = document.getElementById('name').value;
+            let price = document.getElementById('price').value;
+            
+            for(let i=0;i<arr.length;i++){
+                if(arr[i].name==name){
+                    arr[i].price=price;
+                }
+            }
+
+            display()
+
+        }
+
+        function deleteFun(){
+            let name = document.getElementById('name').value;
+            let price = document.getElementById('price').value;
+        
+            for(let i=0;i<arr.length;i++){
+                if(arr[i].name==name && arr[i].price==price){
+                    arr.splice(i,1)
+                }
+            }
+           
+            display()
+        }
+   
